a660b94be53b4840c8b24e534051d2ee5b25cd47
Joel Oliver <joel.oliver@live.com>
2020-11-14 14:43:37 -0800

    Updated the output. Still some errors.

--- debuff_contribution/debuff.py
+++ debuff_contribution/debuff.py
@@ -2,6 +2,10 @@ import requests
 import pprint
 import sys
 
+
+def nop():
+    return 0
+
 base_url = 'https://www.warcraftlogs.com/v1'
 fights = '/report/fights'
 tables = '/report/tables/damage-done'
@@ -9,6 +13,8 @@ tables = '/report/tables/damage-done'
 key = sys.argv[1]
 report = '/' + sys.argv[2]
 
+report = '/CZ1X9txqk3KV8fjy'
+#report = '/FBNYw7htHTgaKcxr'
 
 p = {
     'api_key' : key
@@ -33,7 +39,7 @@ for n in raid_encounter:
         continue
     start, end = n['start_time'], n['end_time']
 
-    print(start, end)
+    #print(start, end)
     p =     { 'start' : start,
                'end' : end,
                'api_key' : key}
@@ -43,22 +49,22 @@ for n in raid_encounter:
 
     encounter['physical_debuff'] = False
     encounter['magic_debuff'] = False
-    print("Encounter ID: " +str(n['id']))
+    #print("Encounter ID: " +str(n['id']))
 
     for player in raid_comp:
-        print(player['name'] + ":" + str(player['id']))
+        #print(player['name'] + ":" + str(player['id']))
 
         pid = 0
         if player['type'] == 'NPC':
-            print("NPC found, skipping")
+            #print("NPC found, skipping")
             continue
         for f in player['fights']:
             if f['id'] == n['id']:
-                print("Found Encounter ID in player's fight list: " + str(n['id']))
+                #print("Found Encounter ID in player's fight list: " + str(n['id']))
                 pid = player['id']
                 break
         if pid == 0:
-            print("Player is not in this encounter, skipping")
+            #print("Player is not in this encounter, skipping")
             continue
 
         p = {'start': start,
@@ -68,7 +74,7 @@ for n in raid_encounter:
 
         damage_tables = requests.get(base_url + tables + report, params=p )
 
-        print(damage_tables.url)
+        #print(damage_tables.url)
 
         encounter[pid] = {}
         encounter[pid]['name'] = player['name']
@@ -88,17 +94,17 @@ for n in raid_encounter:
         #print(player['name'])
 
         json = damage_tables.json()['entries']
-        print(json)
+        #print(json)
         for e in json:
-            print("Name: " + str(e['name']))
-            print("Total Damage: " + str(e['total']))
-            print("Spell Type: " + str(e['type']))
+            #print("Name: " + str(e['name']))
+            #print("Total Damage: " + str(e['total']))
+            #print("Spell Type: " + str(e['type']))
 
             if 'subentries' in e.keys():
                 for se in e['subentries']:
-                    print("Name: " + str(se['name']))
-                    print("Total Damage: " + str(se['total']))
-                    print("Spell Type: " + str(se['type']))
+                    #print("Name: " + str(se['name']))
+                    #print("Total Damage: " + str(se['total']))
+                    #print("Spell Type: " + str(se['type']))
                     if se['type'] == 1:
                         encounter[pid]['physical_damage'] = encounter[pid]['physical_damage'] + int(se['total'])
                     else:
@@ -109,39 +115,74 @@ for n in raid_encounter:
                 else:
                     encounter[pid]['magical_damage'] = encounter[pid]['magical_damage'] + int(e['total'])
 
-    pprint.pprint(encounter)
+    #pprint.pprint(encounter)
+
+    length = int( (end - start) / 1000)
+
+
+    magic_contribution = 0
+    magic_benefitees = 0
+    physical_contribution = 0
+    physical_benefitees = 0
+
+
 
   #  encounter['magic_debuff'] = True
+
+    print(n['name'])
+    print("Encounter length", length)
     for p in encounter:
+
         if 'debuff' in str(p):
             continue
-        print(encounter[p]['name'])
+        #print(encounter[p]['name'])
         if encounter['physical_debuff'] == True:
 
             with_buff_damage = encounter[p]['physical_damage']
             with_buff_removed = int( (with_buff_damage)/1.05 )
             buff_contribution = with_buff_damage - with_buff_removed
 
+            physical_contribution += buff_contribution
+
             if not with_buff_damage > 0:
-                print("This player did no physical damage!")
+                #print("This player did no physical damage!")
+                continue
             else:
-                print('Physical damage done with debuff: ',with_buff_damage)
-                print('Physical damage done without the debuff: ',with_buff_removed)
-                print('Physical damage debuff contributed: ', buff_contribution)
+                nop()
+                physical_benefitees += 1
+                #print('Physical damage done with debuff: ',with_buff_damage)
+                #print('Physical damage done without the debuff: ',with_buff_removed)
+                #print('Physical damage debuff contributed: ', buff_contribution)
         if encounter['magic_debuff'] == True:
 
             with_buff_damage = encounter[p]['magical_damage']
             with_buff_removed = int( (with_buff_damage)/1.05 )
             buff_contribution = with_buff_damage - with_buff_removed
 
-            if not with_buff_damage > 0:
-                print("This player did no magical damage damage!")
-            else:
-                print('Magical damage done with debuff: ',with_buff_damage)
-                print('Magical damage done without the debuff: ',with_buff_removed)
-                print('Magical damage debuff contributed: ', buff_contribution)
+            magic_contribution += buff_contribution
 
-    exit()
+            if not with_buff_damage > 0:
+                #print("This player did no magical damage damage!")
+                continue
+            else:
+                nop()
+                magic_benefitees += 1
+                #print('Magical damage done with debuff: ',with_buff_damage)
+                #print('Magical damage done without the debuff: ',with_buff_removed)
+                #print('Magical damage debuff contributed: ', buff_contribution)
+
+
+    print('Total Physical damage debuff contribution:',physical_contribution)
+    print("Physical damage DPS contribution:",(physical_contribution / length))
+    print("The number of sources with a physical damage component", physical_benefitees)
+    print("The average damage gain per source:", (physical_contribution)/physical_benefitees)
+    print("The average damage per second gained per source:", (physical_contribution / length) / (physical_benefitees))
+    print('Total Magic damage debuff contribution:', magic_contribution)
+    print("Magic damage DPS contribution:", (magic_contribution / length))
+    print("The number of sources with a magic damage component:", magic_benefitees)
+    print("The average damage gain per source:",(magic_contribution) / magic_benefitees)
+    print("The average damage per second gained per source:", (magic_contribution / length) / (magic_benefitees))
+
 
 
 
